dae4f190bceb6fc648757cb6b1093a99f5afd95d
刘念07965 <liunian7965@navinfo.com>
2023-12-05 10:43:00 +0800

    ci: 新增 git hook

--- .lintstagedrc.cjs
+++ .lintstagedrc.cjs
@@ -1,7 +1,4 @@
 module.exports = {
-  "*.{md,yml,yaml}": ["prettier --write"],
-  "*.{js,mjs,cjs,jsx,ts,mts,cts,tsx,json,json5,vue}": [
-    "prettier --write",
-    "eslint --fix",
-  ],
+  "*.{md,yml,yaml,json,json5}": ["prettier --write"],
+  "*.{js,mjs,cjs,jsx,ts,mts,cts,tsx,vue}": ["prettier --write", "eslint --fix"],
 };
